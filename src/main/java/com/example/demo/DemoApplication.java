a47da20bce3fcdc0667b9946f47d946482713b55
mohamed-jebali <mohamedjebali992010@gmail.com>
2024-01-14 14:17:14 +0100

    commit example with intelliJ

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
+
+		for(int i = 0; i <= 10; i++){
+			if(i % 2 == 0){
+				System.out.println("i numeri pari: " + i);
+			}
+			else {
+				System.out.println("i numeri dispari: " + i);
+			}
+
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
