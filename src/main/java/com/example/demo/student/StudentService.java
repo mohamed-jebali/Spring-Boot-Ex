1cb4790c4c572ea6c2dc575f95669a0cd6012c6d
mohamed-jebali <mohamedjebali992010@gmail.com>
2024-01-15 23:22:42 +0100

    student json file saved in StudentService method .save

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -28,6 +29,6 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student){
-        System.out.println(student);
+        studentRepository.save(student);
     }
 }
